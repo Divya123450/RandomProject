0a0f524905d084a0cddd6d6f6693428cfab53e9a
Divya123450 <sahoodivyalochan19@gmail.com>
2025-02-03 14:50:23 +0530

    commit message

---
+++ src/test/java/TestNG/CompanyDetails.java
@@ -0,0 +1,20 @@
+package TestNG;
+import org.testng.annotations.*;
+
+public class CompanyDetails {
+	@Test
+	public void MaleEmployee() 
+	{
+		System.out.println("Some Male person are coming medium background");
+	}
+	
+	@Test
+	public void FemaleEmployee() 
+	{
+		System.out.println("Some Female are coming poor family background");
+	}
+	
+	
+	
+
+}
